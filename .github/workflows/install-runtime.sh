565525cfc97feae76ab561598f9ae4e7dabf23eb
Scott Andrews <sandrews@pivotal.io>
2019-11-21 16:28:09 -0500

    Use updated no-resoruce-request webhook

--- .github/workflows/install-runtime.sh
+++ .github/workflows/install-runtime.sh
@@ -30,7 +30,14 @@ sleep 5
 wait_pod_selector_ready app=cert-manager cert-manager
 wait_pod_selector_ready app=webhook cert-manager
 
-source $FATS_DIR/macros/no-resource-requests.sh
+#TODO: change back to FATS after it is with cert-manager v0.11.0
+# source $FATS_DIR/macros/no-resource-requests.sh
+if [ $(kubectl get nodes -oname | wc -l) = "1" ]; then
+  echo "Eliminate pod resource requests"
+  fats_retry kubectl apply -f https://storage.googleapis.com/projectriff/no-resource-requests-webhook/no-resource-requests-webhook-20191121210956-521ae2a8c3323540.yaml
+  wait_pod_selector_ready app=webhook no-resource-requests
+fi
+#TODO: ^^^
 
 echo "Installing kpack"
 fats_retry kubectl apply -f https://storage.googleapis.com/projectriff/internal/kpack/kpack-0.0.5-snapshot-5a4e635d.yaml
